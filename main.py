a76477ced6d93bbbd447eb3048ab1fdfdfea1acb
Ibadurrahman <shaikhibadurr@gmail.com>
2021-01-21 12:17:51 +0530

    All added

---
+++ main.py
@@ -0,0 +1,8 @@
+import pywhatkit
+pywhatkit.info("Makkah",lines=30) ## This will give the information about makkah by wikipidea
+pywhatkit.image_to_ascii_art("asadedited.png",output_file="pywhatkit_asciiart2.txt") ## This will convert an image in ascii art
+pywhatkit.text_to_handwriting("asadedited",rgb=[255,0,0])## this will give an handwritten copy of given text
+pywhatkit.sendwhatmsg("+916261079111","HII",12,13) ## This will send the message to given number
+
+
+
